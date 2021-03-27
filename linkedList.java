bf09e4e85126457550057d7617352661013fd6c3
Mahmoud Hosseini <79748621+b0o0m@users.noreply.github.com>
2021-03-27 22:39:16 +0430

    Added java doc

--- linkedList.java
+++ linkedList.java
@@ -1,10 +1,16 @@
+package com.example;
+
+
 import java.util.*;
 
 public class linkedList {
+
+    // Fields 
     Node head;
     private int middleElement;
     private int size;
-
+     
+    // this is the node class 
     class Node {
         int data;
         Node next;
@@ -14,7 +20,8 @@ public class linkedList {
             this.next = null;
         }
     }
-
+    
+    /* setter and getter */
     public int getMiddleElement() {
         return middleElement;
     }
@@ -31,6 +38,11 @@ public class linkedList {
         return size;
     }
 
+     /**
+     * This method returns the length of
+     * linked list, and it fix the size field
+     * @return count is the length of it
+     */
     public int size() {
         Node temp = head;
         int count = 0;
@@ -41,54 +53,68 @@ public class linkedList {
         setSize(count);
         return count;
     }
-
+    
+    /**
+    * This method give us the node 
+    * by index
+    */
     public int get(int index) {
         Node current = head;
-        int count = 0; /* index of Node we are
-                          currently looking at */
+        int count = 0; // index of current node 
         while (current != null) {
             if (count == index)
                 return current.data;
             count++;
             current = current.next;
         }
-
-        /* if we get to this line, the caller was asking
-        for a non-existent element so we assert fail */
-        assert (false);
-        return 0;
+        return -1;
     }
-
+    
+    /**
+    * This method removes node by index 
+    * if it was's a valid input does nothing!
+    * @parameter i is the input index
+    */
     public void removeIndex(int i) {
         Node temp = head;
         Node previos = null;
 
         // If head node was the input
         if (temp != null && temp.data == i) {
-            head = temp.next; // Changed head
+            head = temp.next;
             return;
         }
 
-        // Search for the key to be deleted, keep track of
-        // the previous node as we need to change temp.next
         while (temp != null && temp.data != i) {
             previos = temp;
             temp = temp.next;
         }
 
-        // If key was not present in linked list
+        // If key was not present in linkedlist
         if (temp == null)
             return;
 
         previos.next = temp.next;
     }
-
+    
+    /**
+    * This method adds the input 
+    * to first of the linkedList
+    * @parameter k is the data to add 
+    */
     public void addFirst(int k) {
         Node new_node = new Node(k);
         new_node.next = head;
         head = new_node;
     }
-
+    
+    
+    
+    /**
+    * This method add the input 
+    * to the last of linkedList
+    * @parameter k is the node to add Last  
+    */
     public void add(int k) {
 
         Node new_node = new Node(k);
@@ -105,6 +131,11 @@ public class linkedList {
             last = last.next;
         last.next = new_node;
     }
+    
+    /**
+    * This method print nodes 
+    * of the linkedList
+    */
     public void print()
     {
         Node tnode = head;
@@ -113,7 +144,11 @@ public class linkedList {
             tnode = tnode.next;
         }
     }
-
+    
+    /**
+    * This method finds the middle 
+    * index of a linked list
+    */
     public void middleSize() {
         if (size() % 2 == 0) {
             setSize((size() / 2) - 2);
@@ -121,19 +156,30 @@ public class linkedList {
             setSize(size() / 2);
         }
     }
-
+    
+    /**
+    * This method sets middleElement field 
+    */
     public void findMiddle() {
         middleSize();
         setMiddleElement(get(size));
         size() ;
     }
-
+    
+    /**
+    * This method removes middleElement
+    */
     public void removeMiddle() {
         middleSize();
         removeIndex(getSize());
         size() ;
     }
 
+    /**
+    * This method gets the input,if it
+    * contians in linkedList it prints 
+    * the index of it,else it prints -1
+    */
     public void contains(Node head, int x)
     {
         int index = 0  ;
